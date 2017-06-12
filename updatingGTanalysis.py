516b1ce6a682fdf08a73cc6e336749c3c364e586
Garrett Thompson <garett.leland.thompson@gmail.com>
2017-06-12 10:26:00 -0700

    changed tabs and spaces

--- updatingGTanalysis.py
+++ updatingGTanalysis.py
@@ -2,7 +2,7 @@
 # Created by Garrett Thompson
 # Graphical User Interface for Data Analysis
 # Created at Northern Arizona University
-#	for use in the Astrophysical Ice Laboratory
+#       for use in the Astrophysical Ice Laboratory
 # Advisors: Jennifer Hanley, Will Grundy, Henry Roe
 # garrett.leland.thompson@gmail.com
 ##############################################################################
@@ -146,75 +146,75 @@ def choose_files(folder_to_save):
 
 # assumes a csv file, as all data stored from ice lab is in CSV format
 def import_data(filename):
-	raw_data = np.loadtxt(open(filename,"rb"),delimiter=",")
-	xdat = raw_data[:,0]
-	ydat = raw_data[:,1]
-	return xdat,ydat
+        raw_data = np.loadtxt(open(filename,"rb"),delimiter=",")
+        xdat = raw_data[:,0]
+        ydat = raw_data[:,1]
+        return xdat,ydat
 
 
 def freq_click(event, args_list):
-	# if button_click = left: add left line
-	# if button_click = middle: removes closest line
-	# if button_lick = right: finish
-	# add clicked data points to list
+        # if button_click = left: add left line
+        # if button_click = middle: removes closest line
+        # if button_lick = right: finish
+        # add clicked data points to list
         frq_x,fft_ybg,plot_figure,plot_axis,vert_lines, filt_y, filt_ybg,folder_to_save, raw_x = args_list
 
-	plt.xlim(plt.gca().get_xlim())
-	plt.ylim(plt.gca().get_ylim())
-	if event.button==1: 
-		vert_lines.append(event.xdata)
-		plot_axis.plot(frq_x,np.log(np.abs(fft_ybg)),color='blue')
-		#plt.axvline(x=vert_lines[-1],color='black')
+        plt.xlim(plt.gca().get_xlim())
+        plt.ylim(plt.gca().get_ylim())
+        if event.button==1: 
+                vert_lines.append(event.xdata)
+                plot_axis.plot(frq_x,np.log(np.abs(fft_ybg)),color='blue')
+                #plt.axvline(x=vert_lines[-1],color='black')
                 for val in vert_lines:
-		    plt.axvline(x=val,color='black')
-		plt.xlabel('Cycles/Wavenumber')
-		plt.ylabel('Relative Intensity')
-		# draws points as they are added
-		plt.draw()
-	if event.button==2:
+                    plt.axvline(x=val,color='black')
+                plt.xlabel('Cycles/Wavenumber')
+                plt.ylabel('Relative Intensity')
+                # draws points as they are added
+                plt.draw()
+        if event.button==2:
                 # middle click, remove closest vertical line
-		print ('pop!')
-		# gets x,y limits of graph,saves them before destroying figure
-		xlims = plt.gca().get_xlim()
-		ylims = plt.gca().get_ylim()
-		# clears axes, to get rid of old scatter points
-		plot_axis.cla()
-		# re-plots spectrum
-		plot_axis.plot(frq_x,np.log(np.abs(fft_ybg)),color='blue')
-		# sets axes limits to original values
-		plt.xlim(xlims)
-		plt.ylim(ylims)
-		plt.xlabel('Cycles/Wavenumber')
-		plt.ylabel('Relative Intensity')
-		# deletes point closest to mouse click
-		xindx = np.abs(vert_lines-event.xdata).argmin()
-		del vert_lines[xindx]
-		for line in vert_lines:
-			plt.axvline(x=line,color='black')
-		# draws the new set of vertical lines
-		plt.draw()
-	if event.button==3:
+                print ('pop!')
+                # gets x,y limits of graph,saves them before destroying figure
+                xlims = plt.gca().get_xlim()
+                ylims = plt.gca().get_ylim()
+                # clears axes, to get rid of old scatter points
+                plot_axis.cla()
+                # re-plots spectrum
+                plot_axis.plot(frq_x,np.log(np.abs(fft_ybg)),color='blue')
+                # sets axes limits to original values
+                plt.xlim(xlims)
+                plt.ylim(ylims)
+                plt.xlabel('Cycles/Wavenumber')
+                plt.ylabel('Relative Intensity')
+                # deletes point closest to mouse click
+                xindx = np.abs(vert_lines-event.xdata).argmin()
+                del vert_lines[xindx]
+                for line in vert_lines:
+                        plt.axvline(x=line,color='black')
+                # draws the new set of vertical lines
+                plt.draw()
+        if event.button==3:
                 # right click, ends clicking awareness 
-		# plot_figure.canvas.mpl_disconnect(frq_cid)
+                # plot_figure.canvas.mpl_disconnect(frq_cid)
                 os.chdir(folder_to_save)
-		plt.savefig('FFT_filter.eps')
-		with open("freq_window.csv", "w") as f:
-			writer = csv.writer(f)
-			writer.writerow(["Xposition of vert. line"])
-			writer.writerows(zip(vert_lines))
+                plt.savefig('FFT_filter.eps')
+                with open("freq_window.csv", "w") as f:
+                        writer = csv.writer(f)
+                        writer.writerow(["Xposition of vert. line"])
+                        writer.writerows(zip(vert_lines))
                 os.chdir('..')
-		# first window
+                # first window
                 args_dict ={"vert_lines":vert_lines,"frq_x":frq_x,"filt_y":filt_y,"filt_ybg":filt_ybg}
                 plt.close("all")
                 argslist =  [vert_lines,frq_x,filt_y,filt_ybg] 
-     	        filt_y,filt_ybg = window_filter(argslist)
-	        fft_calc(filt_y, filt_ybg, raw_x,folder_to_save)
+                filt_y,filt_ybg = window_filter(argslist)
+                fft_calc(filt_y, filt_ybg, raw_x,folder_to_save)
 
 def fft_calc(filt_y, filt_ybg, raw_x,folder_to_save):
-	# dividing filtered y data from filtered bg data
-	norm_fft = ifft(filt_y)/ifft(filt_ybg)
+        # dividing filtered y data from filtered bg data
+        norm_fft = ifft(filt_y)/ifft(filt_ybg)
         save_as_csv(folder_to_save,"fft_data.csv","raw_x","fft_filt",raw_x,norm_fft.real)
-	plot_data(raw_x,norm_fft.real,folder_to_save)
+        plot_data(raw_x,norm_fft.real,folder_to_save)
 
 
 
@@ -228,153 +228,153 @@ def sgf_calc(folder_to_save):
         with open("sgf_params.txt", "w") as sgf_file:
                 sgf_file.write("Window parameter used: {} \n".format(window_param))
                 sgf_file.write("Polynomial paramter used: {}".format(poly_param))
-	#global norm_smooth
-	smoothed_y = sgf(raw_y,window_param,poly_param,delta=(abs(raw_y)[1]-raw_y)[0])
-	smoothed_ybg =sgf(raw_ybg,window_param,poly_param,delta=(abs(raw_ybg)[1]-raw_ybg)[0])
-	# dividing filtered y data from filtered bg data
-	norm_smooth = smoothed_y / smoothed_ybg
-	rows = zip(raw_x,norm_smooth)
-	with open("sgf_data.csv", "w") as f:
-		writer = csv.writer(f)
-		writer.writerow(["window","polynomail order"])
-		writer.writerow([window_param,poly_param])
-		writer.writerow(["raw_x","sgf_filt"])
-		writer.writerows(rows)
+        #global norm_smooth
+        smoothed_y = sgf(raw_y,window_param,poly_param,delta=(abs(raw_y)[1]-raw_y)[0])
+        smoothed_ybg =sgf(raw_ybg,window_param,poly_param,delta=(abs(raw_ybg)[1]-raw_ybg)[0])
+        # dividing filtered y data from filtered bg data
+        norm_smooth = smoothed_y / smoothed_ybg
+        rows = zip(raw_x,norm_smooth)
+        with open("sgf_data.csv", "w") as f:
+                writer = csv.writer(f)
+                writer.writerow(["window","polynomail order"])
+                writer.writerow([window_param,poly_param])
+                writer.writerow(["raw_x","sgf_filt"])
+                writer.writerows(rows)
         os.chdir('..')
-	return raw_x,norm_smooth
+        return raw_x,norm_smooth
 
 # range of frequenices to cut out
 def window_filter(args_list):
         vert_lines, frq_x, filt_y, filt_ybg = args_list
         window_min, window_max= vert_lines[-2], vert_lines[-1]
-	for i in range(len(frq_x)):
-		if (frq_x[i] >= window_min and frq_x[i] <=window_max) or (frq_x[i]>-1*window_max and frq_x[i]<-1*window_min):
-			filt_y[i] = 0
-			filt_ybg[i] = 0
+        for i in range(len(frq_x)):
+                if (frq_x[i] >= window_min and frq_x[i] <=window_max) or (frq_x[i]>-1*window_max and frq_x[i]<-1*window_min):
+                        filt_y[i] = 0
+                        filt_ybg[i] = 0
         return filt_y,filt_ybg
 
 
 def plot_data(x,y,folder_to_save):
         plot_figure,plot_axis = plotting_data_for_inspection(x,y,"Divide and Filtered Spectrum","Wavenumber cm-1","Relative Intensity","dv_filt_spectrum.pdf",folder_to_save, False)
-	order = str(raw_input('Zoom to liking and then enter what order polynomial for continuum fit\n:'))
+        order = str(raw_input('Zoom to liking and then enter what order polynomial for continuum fit\n:'))
         xcoords,ycoords = [],[]
-	# tells python to turn on awareness for button presses
+        # tells python to turn on awareness for button presses
         global cid
         cid = plot_figure.canvas.mpl_connect('button_press_event', lambda event: onclick(event, [xcoords,ycoords,plot_figure,plot_axis,order,folder_to_save,x,y]))
-	print 'Left to add, middle to remove nearest, and right to finish'
-	plt.show()
+        print 'Left to add, middle to remove nearest, and right to finish'
+        plt.show()
 
 # for creating continuum fit to divide out
 def onclick(event,argslist):
         xcoords,ycoords,plot_figure,plot_axis,order,folder_to_save,x,y = argslist
-	global pvals
-	if event.button==1: 
+        global pvals
+        if event.button==1: 
                 # left click
-        	plt.xlim(plt.gca().get_xlim())
-        	plt.ylim(plt.gca().get_ylim())
+                plt.xlim(plt.gca().get_xlim())
+                plt.ylim(plt.gca().get_ylim())
                 #plt.cla()
-		try:
-			# only delete if curve_fit line already drawn
-			if len(plot_axis.lines) !=1: plot_axis.lines.remove(plot_axis.lines[-1])
-		except: UnboundLocalError
-		# add clicked data points to list
-		xcoords.append(event.xdata)
-		ycoords.append(event.ydata)
-		plot_axis.scatter(xcoords,ycoords,color='black')
-		plt.xlabel('Wavenumber cm-1')
-		plt.ylabel('Relative Intensity')
-		plt.draw()
-		xvals = np.array(xcoords)
-		yvals = np.array(ycoords)
-		# fits values to polynomial, rankwarning is irrelevant
-		warnings.simplefilter('ignore', np.RankWarning)
-		p_fit = np.polyfit(xvals,yvals,order)
-		pvals = np.poly1d(p_fit)
-		plot_axis.plot(x,pvals(x),color='black')
+                try:
+                        # only delete if curve_fit line already drawn
+                        if len(plot_axis.lines) !=1: plot_axis.lines.remove(plot_axis.lines[-1])
+                except: UnboundLocalError
+                # add clicked data points to list
+                xcoords.append(event.xdata)
+                ycoords.append(event.ydata)
+                plot_axis.scatter(xcoords,ycoords,color='black')
+                plt.xlabel('Wavenumber cm-1')
+                plt.ylabel('Relative Intensity')
                 plt.draw()
-		# plt.show(block=False)
-	if event.button==2:
+                xvals = np.array(xcoords)
+                yvals = np.array(ycoords)
+                # fits values to polynomial, rankwarning is irrelevant
+                warnings.simplefilter('ignore', np.RankWarning)
+                p_fit = np.polyfit(xvals,yvals,order)
+                pvals = np.poly1d(p_fit)
+                plot_axis.plot(x,pvals(x),color='black')
+                plt.draw()
+                # plt.show(block=False)
+        if event.button==2:
                 # middle click, remove closest point to click
-		print ('pop!')
-		# gets x,y limits of graph,saves them before destroying figure
-		xlims = plt.gca().get_xlim()
-		ylims = plt.gca().get_ylim()
-		# clears axes, to get rid of old scatter points
-		plot_axis.cla()
-		# re-plots spectrum
-		plot_axis.plot(x,y)
-		# sets axes limits to original values
-		plt.xlim(xlims)
-		plt.ylim(ylims)
-		plt.xlabel('Wavenumber cm-1')
-		plt.ylabel('Relative Intensity')
-		# deletes point closest to mouse click
-		xindx = np.abs(xcoords-event.xdata).argmin()
-		del xcoords[xindx]
-		yindx = np.abs(ycoords-event.ydata).argmin()
-		del ycoords[yindx]
-		# draws the new set of scatter points, and colors them
-		plot_axis.scatter(xcoords,ycoords,color='black')
-		plt.draw()
-		xvals = np.array(xcoords)
-		yvals = np.array(ycoords)
-		# fits values to polynomial, rankwarning is ignored
-		warnings.simplefilter('ignore', np.RankWarning)
-		p_fit = np.polyfit(xvals,yvals,order)
-		pvals = np.poly1d(p_fit)
-		plot_axis.plot(x,pvals(x),color='black')
-		plt.draw()
-	if event.button==3:
-		# right click,ends clicking awareness
-		plot_figure.canvas.mpl_disconnect(cid)
+                print ('pop!')
+                # gets x,y limits of graph,saves them before destroying figure
+                xlims = plt.gca().get_xlim()
+                ylims = plt.gca().get_ylim()
+                # clears axes, to get rid of old scatter points
+                plot_axis.cla()
+                # re-plots spectrum
+                plot_axis.plot(x,y)
+                # sets axes limits to original values
+                plt.xlim(xlims)
+                plt.ylim(ylims)
+                plt.xlabel('Wavenumber cm-1')
+                plt.ylabel('Relative Intensity')
+                # deletes point closest to mouse click
+                xindx = np.abs(xcoords-event.xdata).argmin()
+                del xcoords[xindx]
+                yindx = np.abs(ycoords-event.ydata).argmin()
+                del ycoords[yindx]
+                # draws the new set of scatter points, and colors them
+                plot_axis.scatter(xcoords,ycoords,color='black')
+                plt.draw()
+                xvals = np.array(xcoords)
+                yvals = np.array(ycoords)
+                # fits values to polynomial, rankwarning is ignored
+                warnings.simplefilter('ignore', np.RankWarning)
+                p_fit = np.polyfit(xvals,yvals,order)
+                pvals = np.poly1d(p_fit)
+                plot_axis.plot(x,pvals(x),color='black')
+                plt.draw()
+        if event.button==3:
+                # right click,ends clicking awareness
+                plot_figure.canvas.mpl_disconnect(cid)
                 os.chdir(folder_to_save)
-		plt.savefig('continuum_chosen.pdf')
-		# Saving polynomial eqn used in continuum divide for reference
-		with open("continuum_polynomial.txt", "w") as save_file:
-			save_file.write("%s *x^ %d  " %(pvals[0],0))
-			for i in (xrange(len(pvals))):
-				save_file.write("+ %s *x^ %d  " %(pvals[i+1],i+1))
+                plt.savefig('continuum_chosen.pdf')
+                # Saving polynomial eqn used in continuum divide for reference
+                with open("continuum_polynomial.txt", "w") as save_file:
+                        save_file.write("%s *x^ %d  " %(pvals[0],0))
+                        for i in (xrange(len(pvals))):
+                                save_file.write("+ %s *x^ %d  " %(pvals[i+1],i+1))
                 os.chdir('..')
                 calc_coeffs(pvals,x,y,folder_to_save)
 
 def calc_coeffs(pvals,x,y,folder_to_save):
-	fit_y = pvals(x)
-	# flattens the continuum
-	new_continuum = y / fit_y
-	thickness = int(raw_input('\nEnter thickness of cell in cm\n'))
+        fit_y = pvals(x)
+        # flattens the continuum
+        new_continuum = y / fit_y
+        thickness = int(raw_input('\nEnter thickness of cell in cm\n'))
         # 2 cm thickness for our work in 2016
-	# remove runtime errors when taking negative log and dividing
-	err_settings = np.seterr(invalid='ignore')
-	alpha_coeffs = -np.log(new_continuum) / thickness
+        # remove runtime errors when taking negative log and dividing
+        err_settings = np.seterr(invalid='ignore')
+        alpha_coeffs = -np.log(new_continuum) / thickness
         plotting_data_for_inspection(x,alpha_coeffs,"Alpha Coefficients","Wavenumber cm-1","Absorption cm-1","alpha_coeffs.pdf",folder_to_save,False)
         save_as_csv(folder_to_save,"alpha_coeffs.csv","x","alpha",x,alpha_coeffs)
 
-	# creating masks around each peak
-	x_mask1 = x[(x>10000) & (x<10500)]
-	x_mask2 = x[(x>11200) & (x<12000)]
+        # creating masks around each peak
+        x_mask1 = x[(x>10000) & (x<10500)]
+        x_mask2 = x[(x>11200) & (x<12000)]
         y_mask1 = alpha_coeffs[(x>10000) & (x<10500)]
         y_mask2 = alpha_coeffs[(x>11200) & (x<12000)]
 
-	# writing data for plotting later
+        # writing data for plotting later
         save_as_csv(folder_to_save,"10000_peak.csv","x","y",x_mask1,y_mask1)
         save_as_csv(folder_to_save,"11200_peak.csv","x","y",x_mask2,y_mask2)
 
         # integrated area calcs
-	area10000=trapz(y_mask1,x_mask1)
-	area11200=trapz(y_mask2,x_mask2)
+        area10000=trapz(y_mask1,x_mask1)
+        area11200=trapz(y_mask2,x_mask2)
         os.chdir(folder_to_save)
-	with open("10000area.txt","w") as f:
-		f.write(str(area10000))
-	with open("11200area.txt","w") as f:
-		f.write(str(area11200))
+        with open("10000area.txt","w") as f:
+                f.write(str(area10000))
+        with open("11200area.txt","w") as f:
+                f.write(str(area11200))
         os.chdir('..')
-	finish_prog = raw_input("Press 'y' when finished\n:")
-	check = True
-	while check:
-		if (finish_prog =="y"): check = False
-	plt.close('all')
-	print "Finished!"
-	quit() # end of program
+        finish_prog = raw_input("Press 'y' when finished\n:")
+        check = True
+        while check:
+                if (finish_prog =="y"): check = False
+        plt.close('all')
+        print "Finished!"
+        quit() # end of program
 
 
 if __name__ == '__main__':
